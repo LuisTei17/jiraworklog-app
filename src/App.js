6019a5cf0085a8b71b6b791c993452153f56e719
Luis Teixeira <luisedteixeira@gmail.com>
2019-06-17 11:31:09 -0300

    Showing projects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import Login from './components/login/login';
+import Login from './components/login/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/login" component={Login} />
+      </Router>
     </div>
   );
 }
